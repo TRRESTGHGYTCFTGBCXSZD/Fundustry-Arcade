1001935fd1b4f84162ee8209426cf070991c4d2a
TRRESTGHGYTCFTGBCXSZD <40109698+TRRESTGHGYTCFTGBCXSZD@users.noreply.github.com>
2022-04-26 15:28:56 +0900

    let's load please

---
+++ src/fundustry-arcade/FundustryArcade.java
@@ -0,0 +1,22 @@
+package fundustryarcade;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class FundustryArcade extends Mod{
+
+    public FundustryArcade(){
+
+    }
+
+    @Override
+    public void loadContent(){
+    }
+
+}
